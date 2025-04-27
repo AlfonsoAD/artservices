df21f166179704f1dc06cdf088f5db3acf58f18d
AlfonsoAD <alfonsoad097@gmail.com>
2025-04-27 17:49:03 -0500

    Add EmailJS integration and update contact form

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -9,4 +9,11 @@ export const headerMenu = [
     { name: 'IT', link: '#software-development' },
 ];
 
-export const footerMenu = [{ name: 'Style Guide', link: '/style-guide' }];
+export const footerMenu = [
+    { name: 'Nosotros', link: '#about' },
+    { name: 'Oferta', link: '#offer' },
+    { name: 'Maquinados', link: '#plot' },
+    { name: 'Servicio de Diseño', link: '#design-services' },
+    { name: 'Servicio Industriales', link: '#industrial-services' },
+    { name: 'IT', link: '#software-development' },
+];
